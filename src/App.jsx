c7d8d61e2031a3202e459ea84f85721835e86f08
Jeanlendol <jean.lendol@gmail.com>
2025-07-28 19:43:20 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Dashboard } from './pages/Dashboard'
 import { Home } from './pages/Home'
-import './App.css'
+
 
 function App() {
   return (
