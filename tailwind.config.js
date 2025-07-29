51ce2095800b292360cfbce89c6954f0ce5899e5
Jeanlendol <jean.lendol@gmail.com>
2025-07-28 23:59:07 -0300

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [], // ← agora vazio ou com outros plugins válidos
-}
